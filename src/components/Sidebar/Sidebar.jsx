80c72fb6717781969e79ec7e622fef7a437687ad
DK <darshankhot@programmer.net>
2022-12-30 16:20:45 +0530

    fetching actual teams game data from api to set to table and sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,9 @@ function Sidebar(props) {
 
     <section className="data-box">
       <header className="data-box-header">
-        <div className="team-name">Celtics</div>
+        <div className="team-name">
+          {props.data?.home_team?.name || "NA"}
+        </div>
         <button className="close-sidebar" onClick={props.toggleSidebar}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -14,7 +16,53 @@ function Sidebar(props) {
         </button>
       </header>
 
+      <section className="data-box-info">
+        <div className="data-row">
+          <div className="data-cell">Team Full Name</div>
+          <div className="data-cell">
+            {props.data?.home_team?.full_name || "NA"}
+          </div>
+        </div>
+        <div className="data-row">
+          <div className="data-cell">Total Games in 2021</div>
+          <div className="data-cell">
+            {props.data?.period || "NA"}
+          </div>
+        </div>
 
+        <div className="section-heading">Random Game Details:</div>
+        <div className="data-row">
+          <div className="data-cell bold">Date</div>
+          <div className="data-cell bold">
+            {props.data?.date?.substr(0, 10) || "NA"}
+          </div>
+        </div>
+        <div className="data-row">
+          <div className="data-cell bold">Home Team</div>
+          <div className="data-cell bold">
+            {props.data?.home_team?.name || "NA"}
+          </div>
+        </div>
+        <div className="data-row">
+          <div className="data-cell bold">Home Team Score</div>
+          <div className="data-cell bold">
+            {props.data?.home_team_score || "NA"}
+          </div>
+        </div>
+        <div className="data-row">
+          <div className="data-cell bold">Visitor Team</div>
+          <div className="data-cell bold">
+            {props.data?.visitor_team?.name || "NA"}
+          </div>
+        </div>
+        <div className="data-row">
+          <div className="data-cell bold">Visitor Team Score</div>
+          <div className="data-cell bold">
+            {props.data?.visitor_team_score || "NA"}
+          </div>
+        </div>
+
+      </section>
     </section>
 
   </aside>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,56 @@
 import "./Home.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // components
 import Sidebar from "../../components/Sidebar/Sidebar";
 
 function Home() {
   const [showSidebar, setShowSidebar] = new useState(false);
-  function toggleSidebar() {
+  const [gameData, setGameData] = new useState(() => null);
+  const [selectedTeam, setSelectedTeam] = new useState(undefined);
+  function toggleSidebar(teamId) {
+    setSelectedTeam(teamId);
+    if (typeof teamId === 'number') {
+      setGameData(() => null);
+      fetch(`https://www.balldontlie.io/api/v1/games?per_page=100&team_ids=${teamId}`)
+        .then(res => res.json())
+        .then(data => {
+          return data.data.find(game => game?.home_team.id === teamId)
+        })
+        .then(setGameData);
+    }
     setShowSidebar(val => !val);
   }
+
+  const [pageData, setPageData] = useState(() => ({
+    "total_pages": 3,
+    "current_page": 1,
+    "next_page": 2,
+    "per_page": 10,
+    "total_count": 30
+  }));
+
+  const [tableData, setTableData] = new useState(() => []);
+  function prepareTableData(pageNumber = 1, itemsPerPage = 10) {
+    return new Promise(function (resolve, reject) {
+      fetch(`https://www.balldontlie.io/api/v1/teams?page=${pageNumber}&per_page=${itemsPerPage}`)
+        .then(res => res.json())
+        .then(teams => {
+          setTableData(() => teams?.data || []);
+          resolve(teams);
+        })
+        .catch(reject);
+    });
+  }
+
+  function changePage(pageNumber = pageData.current_page) {
+    prepareTableData(pageNumber)
+      .then(teams => {
+        setPageData(() => teams.meta);
+      });
+  }
+
+  useEffect(changePage, []);
 
   return <>
     <main className="Home">
@@ -27,72 +69,61 @@ function Home() {
         <table className="teams-data-table">
           <thead>
             <tr>
-              <th>
-                Team Name
-              </th>
-              <th>
-                City
-              </th>
-              <th>
-                Abbreviation
-              </th>
-              <th>
-                Conference
-              </th>
-              <th>
-                Division
-              </th>
+              <th>Team Name</th>
+              <th>City</th>
+              <th>Abbreviation</th>
+              <th>Conference</th>
+              <th>Division</th>
             </tr>
           </thead>
           <tbody>
-            <tr onClick={toggleSidebar}>
-              <td>
-                Hawks
-              </td>
-              <td>
-                Atlanta
-              </td>
-              <td>
-                ATL
-              </td>
-              <td>
-                East
-              </td>
-              <td>
-                Southeast
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Hawks
-              </td>
-              <td>
-                Atlanta
-              </td>
-              <td>
-                ATL
-              </td>
-              <td>
-                East
-              </td>
-              <td>
-                Southeast
-              </td>
-            </tr>
+            {
+              tableData.map((el, idx) =>
+                <tr
+                  className={selectedTeam === el.id ? "selected" : undefined}
+                  key={idx}
+                  onClick={() => toggleSidebar(el.id)}
+                >
+                  <td>{el.name || "NA"}</td>
+                  <td>{el.city || "NA"}</td>
+                  <td>{el.abbreviation || "NA"}</td>
+                  <td>{el.conference || "NA"}</td>
+                  <td>{el.division || "NA"}</td>
+                </tr>
+              )
+            }
           </tbody>
         </table>
       </section>
 
       <section className="pagination">
-        <button>&lt;</button>
-        <button>1</button>
-        <button>4</button>
-        <button>&gt;</button>
+        <button
+          disabled={pageData.current_page === 1}
+          onClick={() => changePage(pageData.current_page - 1)}
+        >&lt;</button>
+
+        <button
+          disabled={pageData.current_page === 1}
+          onClick={() => changePage(1)}
+        >1</button>
+
+        {/* to show current page */}
+        {/* {pageData.current_page} */}
+
+        <button
+          disabled={pageData.current_page === pageData.total_pages}
+          onClick={() => changePage(pageData.total_pages)}
+        >{pageData.total_pages}</button>
+
+        <button
+          disabled={!pageData.next_page}
+          onClick={() => changePage(pageData.current_page + 1)}
+        >&gt;</button>
       </section>
     </main>
 
     {/* sidebar */}
-    <Sidebar state={showSidebar} toggleSidebar={toggleSidebar} />
+    <Sidebar state={showSidebar} data={gameData} toggleSidebar={toggleSidebar} />
   </>
 }
 
