44f8bd1f5e5a7b2432f9ab8078981a47c99c3c28
DK <darshankhot@programmer.net>
2022-12-30 21:23:45 +0530

    added city sorting to teams table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,9 +43,36 @@ function Home() {
         .then(res => res.json())
         .then(teams => {
           setTableData(() => teams?.data || []);
+          setCitySortOrder(false);
           resolve(teams);
         })
         .catch(reject);
+    });
+  }
+
+  const [citySortOrder, setCitySortOrder] = new useState(() => false);
+  /**
+   * toggle the sorting of teams according to city names.
+   */
+  function toggleCitySortOrder() {
+    setCitySortOrder(val => !val);
+    sortTableData();
+  }
+
+  /**
+   * sort array of the object of teams according to city names.
+   * @returns {Array} teams array of objects sorted aphabetically
+   */
+  function sortTableData() {
+    setTableData(teams => {
+      const sortedTeams = teams.sort((a, b) => {
+        if (citySortOrder) {
+          return a.city.localeCompare(b.city);
+        } else {
+          return b.city.localeCompare(a.city);
+        }
+      });
+      return sortedTeams;
     });
   }
 
@@ -92,7 +119,17 @@ function Home() {
           <thead>
             <tr>
               <th>Team Name</th>
-              <th>City</th>
+              <th>
+                City
+                <button
+                  className={`sort-city${citySortOrder ? " invert" : ''}`}
+                  onClick={toggleCitySortOrder}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                    <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                  </svg>
+                </button>
+              </th>
               <th>Abbreviation</th>
               <th>Conference</th>
               <th>Division</th>
