7448106e053b13ed1529accb3818f30cbdfaaee1
DK <darshankhot@programmer.net>
2022-12-31 18:02:58 +0530

    added code cleanup and loader

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,8 @@
 import "./Sidebar.scss";
 
+// components
+import Loader from "../Loader/Loader";
+
 function Sidebar(props) {
   return <aside className={`Sidebar ${props.state ? 'on' : 'off'}`}>
     <button className="overlay" onClick={props.toggleSidebar} />
@@ -7,7 +10,7 @@ function Sidebar(props) {
     <section className="data-box">
       <header className="data-box-header">
         <div className="team-name">
-          {props.data?.home_team?.name || "NA"}
+          {props.data?.home_team?.name || <i>Loading...</i>}
         </div>
         <button className="close-sidebar" onClick={props.toggleSidebar}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
@@ -17,50 +20,58 @@ function Sidebar(props) {
       </header>
 
       <section className="data-box-info">
-        <div className="data-row">
-          <div className="data-cell">Team Full Name</div>
-          <div className="data-cell">
-            {props.data?.home_team?.full_name || "NA"}
-          </div>
-        </div>
-        <div className="data-row">
-          <div className="data-cell">Total Games in 2021</div>
-          <div className="data-cell">
-            {props.data?.period || "NA"}
-          </div>
-        </div>
+        {
+          props.data === 'loading' ?
+            <div className="loader-container">
+              <Loader></Loader>
+            </div> :
+            <>
+              <div className="data-row">
+                <div className="data-cell">Team Full Name</div>
+                <div className="data-cell">
+                  {props.data?.home_team?.full_name || <>NA</>}
+                </div>
+              </div>
+              <div className="data-row">
+                <div className="data-cell">Total Games in 2021</div>
+                <div className="data-cell">
+                  {props.data?.period || <>NA</>}
+                </div>
+              </div>
 
-        <div className="section-heading">Random Game Details:</div>
-        <div className="data-row">
-          <div className="data-cell bold">Date</div>
-          <div className="data-cell bold">
-            {props.data?.date?.substr(0, 10) || "NA"}
-          </div>
-        </div>
-        <div className="data-row">
-          <div className="data-cell bold">Home Team</div>
-          <div className="data-cell bold">
-            {props.data?.home_team?.name || "NA"}
-          </div>
-        </div>
-        <div className="data-row">
-          <div className="data-cell bold">Home Team Score</div>
-          <div className="data-cell bold">
-            {props.data?.home_team_score || "NA"}
-          </div>
-        </div>
-        <div className="data-row">
-          <div className="data-cell bold">Visitor Team</div>
-          <div className="data-cell bold">
-            {props.data?.visitor_team?.name || "NA"}
-          </div>
-        </div>
-        <div className="data-row">
-          <div className="data-cell bold">Visitor Team Score</div>
-          <div className="data-cell bold">
-            {props.data?.visitor_team_score || "NA"}
-          </div>
-        </div>
+              <div className="section-heading">Random Game Details:</div>
+              <div className="data-row">
+                <div className="data-cell bold">Date</div>
+                <div className="data-cell bold">
+                  {props.data?.date?.substr(0, 10) || <>NA</>}
+                </div>
+              </div>
+              <div className="data-row">
+                <div className="data-cell bold">Home Team</div>
+                <div className="data-cell bold">
+                  {props.data?.home_team?.name || <>NA</>}
+                </div>
+              </div>
+              <div className="data-row">
+                <div className="data-cell bold">Home Team Score</div>
+                <div className="data-cell bold">
+                  {props.data?.home_team_score || <>NA</>}
+                </div>
+              </div>
+              <div className="data-row">
+                <div className="data-cell bold">Visitor Team</div>
+                <div className="data-cell bold">
+                  {props.data?.visitor_team?.name || <>NA</>}
+                </div>
+              </div>
+              <div className="data-row">
+                <div className="data-cell bold">Visitor Team Score</div>
+                <div className="data-cell bold">
+                  {props.data?.visitor_team_score || <>NA</>}
+                </div>
+              </div>
+            </>
+        }
 
       </section>
     </section>
